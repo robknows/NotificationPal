9f4f3244ccd73833abe4e3954e9237d191afedc7
Rob <robmoore121@gmail.com>
2017-06-26 03:14:42 +0100

    Parameterising script

--- install-on-emulator.sh
+++ install-on-emulator.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-cd ~/Android/Sdk/emulator && LD_PRELOAD='/usr/lib/x86_64-linux-gnu/libstdc++.so.6' ~/Android/Sdk/emulator/emulator -netdelay none -netspeed full -avd Nexus_5_Edited_API_25 &
-cd ~/StudioProjects/NotificationPal && sleep 20 && ./gradlew installDebug
+
+echo "Starting emulator\n" &&
+cd $ANDROID_HOME/Sdk/emulator &&
+LD_PRELOAD='/usr/lib/x86_64-linux-gnu/libstdc++.so.6' $ANDROID_HOME/Sdk/emulator/emulator -netdelay none -netspeed full -avd ${1:-Nexus_5_Edited_API_25} &
+sleep 25 && # Wait for emulator to finish starting up
+echo "Installing app\n" &&
+./gradlew installDebug
+
