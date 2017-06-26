920957d1ea68b642923f35ada3e28975690b784e
Rob <robmoore121@gmail.com>
2017-06-26 03:15:25 +0100

    Changing default emulator to now have EDITED in the name lol

--- install-on-emulator.sh
+++ install-on-emulator.sh
@@ -2,7 +2,7 @@
 
 echo "Starting emulator\n" &&
 cd $ANDROID_HOME/Sdk/emulator &&
-LD_PRELOAD='/usr/lib/x86_64-linux-gnu/libstdc++.so.6' $ANDROID_HOME/Sdk/emulator/emulator -netdelay none -netspeed full -avd ${1:-Nexus_5_Edited_API_25} &
+LD_PRELOAD='/usr/lib/x86_64-linux-gnu/libstdc++.so.6' $ANDROID_HOME/Sdk/emulator/emulator -netdelay none -netspeed full -avd ${1:-Pixel_API_25} &
 sleep 25 && # Wait for emulator to finish starting up
 echo "Installing app\n" &&
 ./gradlew installDebug
