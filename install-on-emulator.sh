70b58214965dba6db767c2288d5433e3a1b83341
Rob <robmoore121@gmail.com>
2017-07-02 21:50:05 +0100

    Increasing time allocated for emulator start-up

--- install-on-emulator.sh
+++ install-on-emulator.sh
@@ -3,7 +3,7 @@
 echo "Starting emulator\n" &&
 cd $ANDROID_HOME/Sdk/emulator &&
 LD_PRELOAD='/usr/lib/x86_64-linux-gnu/libstdc++.so.6' $ANDROID_HOME/Sdk/emulator/emulator -netdelay none -netspeed full -avd ${1:-Pixel_API_25} &
-sleep 25 && # Wait for emulator to finish starting up
+sleep 40 && # Wait for emulator to finish starting up
 echo "Installing app\n" &&
 ./gradlew installDebug
 
--- run-instrumented-tests.sh
+++ run-instrumented-tests.sh
@@ -5,7 +5,7 @@ then
 echo "Starting emulator" &&
 cd $ANDROID_HOME/Sdk/emulator &&
 LD_PRELOAD='/usr/lib/x86_64-linux-gnu/libstdc++.so.6' $ANDROID_HOME/Sdk/emulator/emulator -netdelay none -netspeed full -avd "${2:-Pixel_API_25}" &
-sleep 35 && # Wait for emulator to finish starting up
+sleep 40 && # Wait for emulator to finish starting up
 echo "Running tests on emulator" &&
 ./gradlew connectedAndroidTest
 elif [ "$1" == already-connected ]
