7ba6dc36a2e6ea3e887912fe886f2720a1179028
Rob <robmoore121@gmail.com>
2017-09-18 18:57:52 +0100

    can change the spinner option in test, and it doesn't break the functionality of the code either.

--- app/src/androidTest/java/com/app/notificationpal/NotificationRegistryTest.java
+++ app/src/androidTest/java/com/app/notificationpal/NotificationRegistryTest.java
@@ -13,11 +13,16 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import static android.content.Context.NOTIFICATION_SERVICE;
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withSpinnerText;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertEquals;
 
 public class NotificationRegistryTest {
@@ -31,7 +36,6 @@ public class NotificationRegistryTest {
     @Test
     public void canRegisterNotification() {
         onView(withId(R.id.notificationName)).perform(replaceText("Train Time")).perform(closeSoftKeyboard());
-
         onView(withId(R.id.submitNotification)).perform(click());
 
         assertEquals("Train Time", notificationManager.getActiveNotifications()[0].getNotification().extras.getString(Notification.EXTRA_TITLE));
@@ -41,10 +45,24 @@ public class NotificationRegistryTest {
     @Test
     public void registersNotificationOnlyOnce() {
         onView(withId(R.id.notificationName)).perform(replaceText("Train Time")).perform(closeSoftKeyboard());
+        onView(withId(R.id.submitNotification)).perform(click());
+        onView(withId(R.id.submitNotification)).perform(click());
+        onView(withId(R.id.submitNotification)).perform(click());
+        onView(withId(R.id.submitNotification)).perform(click());
 
-        onView(withId(R.id.submitNotification)).perform(click());
-        onView(withId(R.id.submitNotification)).perform(click());
-        onView(withId(R.id.submitNotification)).perform(click());
+        assertEquals("Train Time", notificationManager.getActiveNotifications()[0].getNotification().extras.getString(Notification.EXTRA_TITLE));
+        assertEquals(1, notificationManager.getActiveNotifications().length);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Test
+    public void canChangeConstraintType() {
+        String spinnerOptionText = appContext.getResources().getString(R.string.location);
+
+        onView(withId(R.id.notificationName)).perform(replaceText("Train Time")).perform(closeSoftKeyboard());
+        onView(withId(R.id.constraintTypeSpinner)).perform(click());
+        onData(anything()).atPosition(1).perform(click());
+        onView(withId(R.id.constraintTypeSpinner)).check(matches(withSpinnerText(containsString(spinnerOptionText))));
         onView(withId(R.id.submitNotification)).perform(click());
 
         assertEquals("Train Time", notificationManager.getActiveNotifications()[0].getNotification().extras.getString(Notification.EXTRA_TITLE));
